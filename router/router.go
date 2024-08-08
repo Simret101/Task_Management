489427b77c569aa6b2dc6fe3af8058e7450a054b
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-08 05:33:08 +0300

    Update router.go

--- router/router.go
+++ router/router.go
@@ -6,15 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
+// initializes the router and sets up the API routes.
 func SetupRouter() *gin.Engine {
+	// Create a Gin router with default middleware (logger and recovery)
 	r := gin.Default()
 
-	r.GET("/tasks", controllers.GetAllTasks)       
-	r.GET("/tasks/:id", controllers.GetTaskByID)  
-	r.POST("/tasks", controllers.CreateTask)       
-	r.PUT("/tasks/:id", controllers.UpdateTask)  
-	r.DELETE("/tasks/:id", controllers.DeleteTask)
+	// Define the routes and their corresponding controller functions
+	r.GET("/tasks", controllers.GetAllTasks)       // to retrieve all tasks
+	r.GET("/tasks/:id", controllers.GetTaskByID)   // to retrieve a specific task by ID
+	r.POST("/tasks", controllers.CreateTask)       // to create a new task
+	r.PUT("/tasks/:id", controllers.UpdateTask)    // to update an existing task by ID
+	r.DELETE("/tasks/:id", controllers.DeleteTask) // to delete a task by ID
 
 	return r
 }
