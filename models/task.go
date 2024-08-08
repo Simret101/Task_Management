7f503d97247455e812d0443cd5e96e794cd56235
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-08 05:32:10 +0300

    Update task.go

--- models/task.go
+++ models/task.go
@@ -2,10 +2,10 @@ package models
 
 // Task represents a task with its ID, title, description, due date, and status.
 type Task struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	DueDate     string `json:"duedate"`
-	Status      string `json:"status"`
+	ID          int    `json:"id"`          // Unique identifier for the task
+	Title       string `json:"title"`       // Title of the task
+	Description string `json:"description"` // Detailed description of the task
+	DueDate     string `json:"due_date"`    // Due date for the task in YYYY-MM-DD format
+	Status      string `json:"status"`      // Status of the task ( "completed", "inprogress", "started")
 }
 
